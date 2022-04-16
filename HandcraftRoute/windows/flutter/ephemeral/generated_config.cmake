12d631b4d99a3e80227dc90b539a7bec163bd64e
RafaelSalazar22 <91817560+RafaelSalazar22@users.noreply.github.com>
2022-04-16 01:05:17 -0500

    Add files via upload

---
+++ HandcraftRoute/windows/flutter/ephemeral/generated_config.cmake
@@ -0,0 +1,18 @@
+# Generated code do not commit.
+file(TO_CMAKE_PATH "C:\\src\\flutter" FLUTTER_ROOT)
+file(TO_CMAKE_PATH "C:\\Users\\rafae\\3D Objects\\HandcraftRoute (1)\\HandcraftRoute\\handcrafroute" PROJECT_DIR)
+
+# Environment variables to pass to tool_backend.sh
+list(APPEND FLUTTER_TOOL_ENVIRONMENT
+  "FLUTTER_ROOT=C:\\src\\flutter"
+  "PROJECT_DIR=C:\\Users\\rafae\\3D Objects\\HandcraftRoute (1)\\HandcraftRoute\\handcrafroute"
+  "FLUTTER_ROOT=C:\\src\\flutter"
+  "FLUTTER_EPHEMERAL_DIR=C:\\Users\\rafae\\3D Objects\\HandcraftRoute (1)\\HandcraftRoute\\handcrafroute\\windows\\flutter\\ephemeral"
+  "PROJECT_DIR=C:\\Users\\rafae\\3D Objects\\HandcraftRoute (1)\\HandcraftRoute\\handcrafroute"
+  "FLUTTER_TARGET=C:\\Users\\rafae\\3D Objects\\HandcraftRoute (1)\\HandcraftRoute\\handcrafroute\\lib\\main.dart"
+  "DART_DEFINES=RkxVVFRFUl9XRUJfQVVUT19ERVRFQ1Q9dHJ1ZQ=="
+  "DART_OBFUSCATION=false"
+  "TRACK_WIDGET_CREATION=true"
+  "TREE_SHAKE_ICONS=false"
+  "PACKAGE_CONFIG=C:\\Users\\rafae\\3D Objects\\HandcraftRoute (1)\\HandcraftRoute\\handcrafroute\\.dart_tool\\package_config.json"
+)
