d06ee6e07a01061d6029e7cedb3dbfd088d281d1
Eduardo Emanuel Herrera <84540496+E-GLITCH-DOT@users.noreply.github.com>
2022-04-17 11:25:09 -0500

    Add files via upload

---
+++ AURELIO/GarbageR/GarbageR/Controllers/LoginController.cs
@@ -0,0 +1,116 @@
+﻿using GarbageRcomplete3._1.Models;
+using GarbageRcomplete3._1.Models.Request;
+using GarbageRcomplete3._1.Models.Response;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace GarbageRcomplete3._1.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class LoginController : ControllerBase
+    {
+       [HttpPut]
+        public IActionResult Login(Usuario usuario)
+        {
+            Respuesta<string> oRespuesta = new Respuesta<string>();
+            using (FoodCompleteContext db = new FoodCompleteContext())
+
+            {
+                try
+                {
+                    {
+                        TokenClass token = new TokenClass();
+
+                        var obj = db.Usuario.FirstOrDefault(x => x.Correo == usuario.Correo);
+                        if (obj == null)
+                        {
+                            
+                            token.TokenOrMessage = "Correo electronico incorrecto";
+                            oRespuesta.mensaje = token.TokenOrMessage;
+                            return Ok(oRespuesta);
+                        }
+
+                        bool credentials = obj.Password.Equals(usuario.Password);
+                        if (!credentials)
+                        {
+                            token.TokenOrMessage = "Passoword Incorrecto";
+                            oRespuesta.mensaje = token.TokenOrMessage;
+                            return Ok(oRespuesta);
+                        }
+                        
+                            
+                        
+                        obj.Token = TokenManager.GenerateToken(usuario.Correo);
+                        token.TokenOrMessage = obj.Token;
+                        //Indica que se modifico
+                        db.Entry(obj).State = Microsoft.EntityFrameworkCore.EntityState.Modified;
+                        
+                        db.SaveChanges();
+                        oRespuesta.Exito = 1;
+                        token.Success = 1;
+                        token.idUser = obj.IdUser;
+                        token.TypoUser = obj.TypoUsuer;
+                        oRespuesta.Data = obj.Token;
+                        return Ok(oRespuesta);
+                        
+                        
+                       
+
+
+                        //
+                        //oRespuesta.Data = lst;
+                    }
+                }
+                catch (Exception ex)
+                {
+                    oRespuesta.mensaje = ex.Message;
+
+                }
+                return Ok(oRespuesta);
+            }
+        }
+        [HttpGet("Verificar")]
+        //Con este metodo vamos a eliminar cualquiera que querramos
+        public IActionResult Verify( string key)
+        {
+           
+            Respuesta<Usuario> oRespuesta = new Respuesta<Usuario>();
+
+            try
+            {
+                using (FoodCompleteContext db = new FoodCompleteContext())
+                {
+                    //Usuario usuario = new Usuario();
+                    //usuario.Token = key;
+                   
+                    TokenManager token = new TokenManager();
+                    Usuario user = new Usuario();
+                    user.Token = key;
+                    var obj = db.Usuario.Where(x=>x.Token== user.Token).FirstOrDefault();
+                    if (obj != null)
+                    {
+                        oRespuesta.Exito = 1;
+                        oRespuesta.Data = obj;
+                    }
+                    else
+                    {
+                        oRespuesta.mensaje = "No se encontro el Usuario";
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                oRespuesta.mensaje = ex.Message;
+            }
+            return Ok(oRespuesta);
+            }
+        
+       
+        }
+}
---
+++ AURELIO/GarbageR/GarbageR/Models/Request/ReporteRequest.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace GarbageR.Models.Request
+{
+    public class ReporteRequest
+    {
+        public int IdReporte { get; set; }
+        public string NombreReporte { get; set; }
+        public string Descripcion { get; set; }
+        public string EstadoReporte { get; set; } = "Su reporte esta Pendiente";
+        public int? IdUsuario { get; set; }
+        public string FechaReporte { get; set; }
+        public string Direccion { get; set; }
+
+    }
+}
