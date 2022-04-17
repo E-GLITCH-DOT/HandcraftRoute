ab9d12fb94dedbec81cfbbde50335de48c534a50
Eduardo Emanuel Herrera <84540496+E-GLITCH-DOT@users.noreply.github.com>
2022-04-17 11:17:40 -0500

    Add files via upload
    ADD CHANGES

---
+++ Proyecto_Aurelio/GarbageR/GarbageR/Models/BaseENContext.cs
@@ -0,0 +1,126 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata;
+
+namespace GarbageR.Models
+{
+    public partial class BaseENContext : DbContext
+    {
+        public BaseENContext()
+        {
+        }
+
+        public BaseENContext(DbContextOptions<BaseENContext> options)
+            : base(options)
+        {
+        }
+
+        public virtual DbSet<Reporte> Reporte { get; set; }
+        public virtual DbSet<Usuario> Usuario { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            if (!optionsBuilder.IsConfigured)
+            {
+#warning To protect potentially sensitive information in your connection string, you should move it out of source code. See http://go.microsoft.com/fwlink/?LinkId=723263 for guidance on storing connection strings.
+                optionsBuilder.UseSqlServer("Server=DESKTOP-4LR3CD9;Database=BaseEN;Trusted_Connection=True;");
+            }
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Reporte>(entity =>
+            {
+                entity.HasKey(e => e.IdReporte);
+
+                entity.Property(e => e.IdReporte)
+                    .HasColumnName("Id_Reporte")
+                    .ValueGeneratedNever();
+
+                entity.Property(e => e.Descripcion)
+                    .IsRequired()
+                    .HasMaxLength(300)
+                    .IsUnicode(false);
+
+                entity.Property(e => e.Direccion)
+                    .IsRequired()
+                    .HasMaxLength(50)
+                    .IsUnicode(false);
+
+                entity.Property(e => e.EstadoReporte)
+                    .IsRequired()
+                    .HasColumnName("Estado_Reporte")
+                    .HasMaxLength(50);
+
+                entity.Property(e => e.FechaReporte)
+                    .IsRequired()
+                    .HasColumnName("Fecha_Reporte")
+                    .HasMaxLength(50);
+
+                entity.Property(e => e.IdUsuario).HasColumnName("Id_Usuario");
+
+                entity.Property(e => e.NombreReporte)
+                    .IsRequired()
+                    .HasColumnName("Nombre_Reporte")
+                    .HasMaxLength(50)
+                    .IsUnicode(false);
+
+                entity.HasOne(d => d.IdUsuarioNavigation)
+                    .WithMany(p => p.Reporte)
+                    .HasForeignKey(d => d.IdUsuario)
+                    .HasConstraintName("FK_Reporte_Reporte");
+            });
+
+            modelBuilder.Entity<Usuario>(entity =>
+            {
+                entity.HasKey(e => e.IdUsuario);
+
+                entity.Property(e => e.IdUsuario)
+                    .HasColumnName("Id_Usuario")
+                    .ValueGeneratedNever();
+
+                entity.Property(e => e.Apellidos)
+                    .IsRequired()
+                    .HasMaxLength(50)
+                    .IsUnicode(false);
+
+                entity.Property(e => e.Correo)
+                    .IsRequired()
+                    .HasMaxLength(50)
+                    .IsUnicode(false);
+
+                entity.Property(e => e.Direccion)
+                    .IsRequired()
+                    .HasMaxLength(50)
+                    .IsUnicode(false);
+
+                entity.Property(e => e.Genero)
+                    .IsRequired()
+                    .HasMaxLength(50)
+                    .IsUnicode(false);
+
+                entity.Property(e => e.Nombre)
+                    .IsRequired()
+                    .HasMaxLength(50)
+                    .IsUnicode(false);
+
+                entity.Property(e => e.Password)
+                    .IsRequired()
+                    .HasMaxLength(26)
+                    .IsUnicode(false);
+
+                entity.Property(e => e.Telefono)
+                    .IsRequired()
+                    .HasMaxLength(15)
+                    .IsUnicode(false)
+                    .IsFixedLength();
+
+                entity.Property(e => e.TipoUsuario).HasColumnName("Tipo_Usuario");
+            });
+
+            OnModelCreatingPartial(modelBuilder);
+        }
+
+        partial void OnModelCreatingPartial(ModelBuilder modelBuilder);
+    }
+}
---
+++ Proyecto_Aurelio/GarbageR/GarbageR/Models/Usuario.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace GarbageR.Models
+{
+    public partial class Usuario
+    {
+        public Usuario()
+        {
+            Reporte = new HashSet<Reporte>();
+        }
+
+        public int IdUsuario { get; set; }
+        public string Nombre { get; set; }
+        public string Apellidos { get; set; }
+        public string Correo { get; set; }
+        public string Password { get; set; }
+        public string Direccion { get; set; }
+        public string Telefono { get; set; }
+        public string Token { get; set; }
+        public int TipoUsuario { get; set; } = 0;
+        public string Genero { get; set; }
+        public int Edad { get; set; }
+
+        public virtual ICollection<Reporte> Reporte { get; set; }
+    }
+}
